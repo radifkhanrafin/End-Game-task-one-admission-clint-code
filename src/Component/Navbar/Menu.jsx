f7d1e82fbb213acfc0b2117e4fb869137277e0b7
radifkhanrafin <meherafkabir@gmail.com>
2023-07-22 14:30:14 +0600

    first commit

---
+++ src/Component/Navbar/Menu.jsx
@@ -0,0 +1,15 @@
+
+
+const Menu = () => {
+    return (
+        <>
+            <li className="list-none bg-slate-300  px-3 py-1 rounded-md">Home</li >
+            <li className="list-none bg-slate-300  px-3 py-1 rounded-md">Colleges</li >
+            <li className="list-none bg-slate-300  px-3 py-1 rounded-md">Admission</li >
+            <li className="list-none bg-slate-300  px-3 py-1 rounded-md">My College</li >
+            
+        </>
+    );
+};
+
+export default Menu;
